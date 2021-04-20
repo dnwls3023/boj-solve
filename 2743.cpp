0798a23e86875be7703f97714be6831f1d66cc74
dnwls3023 <68366527+dnwls3023@users.noreply.github.com>
2021-04-20 22:39:18 +0900

    Create 2743.cpp

---
+++ 2743.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+/*
+    백준 2743번 : 
+    https://github.com/dnwls3023/
+    
+    접근방법:
+    
+    알고리즘 설명:
+    말이 필요한가?
+*/
+string str;
+int main(void){
+    cin>>str;
+    cout<<str.size()<<endl;
+    return 0;
+}
